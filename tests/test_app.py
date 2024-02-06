caeb70dd6039f0803a51153230fd370b6dcbbaa9
Alisson Demys <Alisson_demys@hotmail.com>
2024-02-05 21:03:35 -0300

    :sparkles: add entity and html route

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+
 from fast_zero.app import app
 
 
@@ -9,3 +10,12 @@ def test_root_deve_retornar_200_e_ola_mundo():
 
     assert response.status_code == 200
     assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_html_deve_retornar_200_e_html():
+    client = TestClient(app)
+
+    response = client.get('/html')
+
+    assert response.status_code == 200
+    assert 'Olá Mundo' in response.text
