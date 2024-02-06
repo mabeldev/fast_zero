5bb163e00d7372af4e29f93132ea5c8f65bf22d5
Alisson Demys <Alisson_demys@hotmail.com>
2024-02-06 11:35:43 -0300

    :sparkles: CRUD User

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,21 +1,97 @@
 from fastapi.testclient import TestClient
-
+import pytest
 from fast_zero.app import app
 
 
-def test_root_deve_retornar_200_e_ola_mundo():
-    client = TestClient(app)
+@pytest.fixture
+def client():
+    return TestClient(app)
 
+
+def test_root_deve_retornar_200_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == 200
     assert response.json() == {'message': 'Olá Mundo!'}
 
 
-def test_html_deve_retornar_200_e_html():
-    client = TestClient(app)
-
+def test_html_deve_retornar_200_e_html(client):
     response = client.get('/html')
 
     assert response.status_code == 200
     assert 'Olá Mundo' in response.text
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+    assert response.status_code == 200
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'alice',
+                'email': 'alice@example.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_update_user_with_invalid_id(client):
+    response = client.put(
+        '/users/0',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == 200
+    assert response.json() == {'message': 'User deleted'}
+
+
+def test_delete_user_with_invalid_id(client):
+    response = client.delete('/users/0')
+
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'User not found'}
