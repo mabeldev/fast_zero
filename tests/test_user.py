e86e7f974f6be378466212f425abeb389c3e8ea8
Alisson Demys <Alisson_demys@hotmail.com>
2024-02-08 08:21:41 -0300

    :sparkles: Atualizando endpoints para usar o banco de dados real

---
+++ tests/test_user.py
@@ -0,0 +1,103 @@
+from fast_zero.schemas import UserPublic
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_create_user_with_existent_username(client, user):
+    UserPublic.model_validate(user).model_dump()
+    response = client.post(
+        '/users',
+        json={
+            'username': user.username,
+            'email': 'teste@test.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == 400
+    assert response.json() == {'detail': 'Username already registered'}
+
+
+def test_get_user(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+
+    response = client.get('/users/1')
+    assert response.status_code == 200
+    assert response.json() == user_schema
+
+
+def test_get_user_with_invalid_id(client):
+    response = client.get('/users/0')
+    assert response.status_code == 404
+    assert response.json() == {
+        'detail': 'User not found',
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users')
+    assert response.status_code == 200
+    assert response.json() == {'users': []}
+
+
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_update_user_with_invalid_id(client):
+    response = client.put(
+        '/users/0',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_delete_user(client, user):
+    response = client.delete('/users/1')
+    assert response.status_code == 200
+    assert response.json() == {'message': 'User deleted'}
+
+
+def test_delete_user_with_invalid_id(client):
+    response = client.delete('/users/0')
+
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'User not found'}
